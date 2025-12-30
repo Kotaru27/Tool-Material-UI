ffe8a72812614dd1bc3d52f4c9d587320c144681
Note <99592731+Kotaru27@users.noreply.github.com>
2025-12-30 20:52:05 +0530

    index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // 1. Import HashRouter
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -10,6 +11,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap App with HashRouter */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
